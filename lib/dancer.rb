aa01eb338a8a7f2c6b9e4c093e814af42e784c6e
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-30 14:15:03 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,6 +5,10 @@ require_relative "./class_methods_module"
 class Dancer
   include Dance       
   extend MetaDancing 
+  
+   extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
   attr_accessor :name
   def initialize(name)
     @name = name
