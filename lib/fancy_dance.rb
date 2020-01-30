a104b9dcc584067a3094c55715ee79597ec65fae
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-30 14:12:24 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,12 +2,12 @@ require_relative "./dance_module"
 require_relative "./class_methods_module"
 
 module FancyDance
-  module InstanceMethods              # you "include" these instance methods
+  module InstanceMethods             
     def superjump 
       "I'm just one jumping instance"
     end
   end
-  module ClassMethods                 # you "extend" these class methods
+  module ClassMethods                
     def megajump 
       "I'm a whole class of MOFO's jumping"
     end
