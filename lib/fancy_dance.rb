c335c0df5eafe5b5ecadc725ab3da7c31da9f0d1
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-30 14:12:40 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -3,8 +3,21 @@ require_relative "./class_methods_module"
 
 module FancyDance
   module InstanceMethods             
-    def superjump 
-      "I'm just one jumping instance"
+    
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
   module ClassMethods                
